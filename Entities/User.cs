a23d0299635db50b4544d9963c2ccc598b610a90
unknown <sasha.lartsev@gmail.com>
2025-07-16 22:30:19 +0400

    In it commit

---
+++ Entities/User.cs
@@ -0,0 +1,16 @@
+﻿namespace Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public int? DiagnosticId { get; set; }
+        public ICollection<Recomdendation> Recomdendations { get; set; }
+        public Diagnostic Diagnostic { get; set; }
+
+        public User(int id, int? diagnosticId) 
+        { 
+            Id = id;
+            DiagnosticId = diagnosticId;
+        }
+    }
+}
