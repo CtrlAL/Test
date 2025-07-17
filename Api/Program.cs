17bdb3fa110da82eb8789f86ccea481adcad71e3
Larcev.Aleksandr <sasha.lartsev@gmail.com>
2025-07-17 18:31:33 +0400

    Свагер

--- Api/Program.cs
+++ Api/Program.cs
@@ -24,11 +24,8 @@ namespace WebApplication1
 
             var app = builder.Build();
 
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
+			app.UseSwagger();
+			app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Nutrition v1"));
 
             app.UseHttpsRedirection();
 
