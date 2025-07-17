b4f90f16da0c8235f0161ba989f64e36e1d73bca
Larcev.Aleksandr <sasha.lartsev@gmail.com>
2025-07-17 18:41:39 +0400

    Model mapper

--- Api/Models/CurrentNutrientConsumptionModel.cs
+++ Api/Models/CurrentNutrientConsumptionModel.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel;
+﻿using Entities.MapTypes;
+using System.ComponentModel;
 
 namespace Api.Models
 {
@@ -15,5 +16,21 @@ namespace Api.Models
 		[Description("Deficiency Diagram")]
 		public float NormDeviationPercent { get; set; }
 		public bool InDeficiency { get; set; }
+
+		public static CurrentNutrientConsumptionModel FromEntity(CurrentNutrientConsumption entity)
+		{
+			return new CurrentNutrientConsumptionModel
+			{
+				Id = entity.Id,
+				Name = entity.Name,
+				CurrentCount = entity.CurrentCount,
+				NormCount = entity.NormCount,
+
+				CurrentPercent = entity.CurrentCount / entity.NormCount,
+				NormDeviationPercent = (entity.NormCount - entity.CurrentCount) / entity.NormCount,
+
+				InDeficiency = entity.CurrentCount < entity.NormCount
+			};
+		}
 	}
 }
