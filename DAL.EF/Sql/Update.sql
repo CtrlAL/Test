d1148940214ab8adef10f89a6caad1859538a878
unknown <sasha.lartsev@gmail.com>
2025-07-18 00:29:57 +0400

    Оставил двс sqlки для создание тестовой таблицы и вставки данных

--- DAL.EF/Sql/Update.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE "ProductPersonalSuggestions" (
-    "ProductId" INT NOT NULL REFERENCES "Products"("Id"),
-    "ProductSuggestionId" INT NOT NULL REFERENCES "PersonalSuggestions"("Id"),
-    PRIMARY KEY ("ProductId", "ProductSuggestionId")
-);
